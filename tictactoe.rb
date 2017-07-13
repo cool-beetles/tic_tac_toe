326b62bd05c17c850f7eaded29f51c45732f849d
Ada <adrianna.kucinska@onet.eu>
2017-07-13 13:24:27 +0200

    #3 add users move

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,3 +1,8 @@
 def tictactoe (string)
   board = ["a1","b1","c1","a2","b2","c2","a3","b3","c3"]
+
+  puts "Gracz X"
+  puts "Podaj miejsce ruchu, np. a1, b3:"
+  selected_move=gets.chomp
+  selected_move.downcase
 end 
