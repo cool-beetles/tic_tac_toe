e9dcd61f014f97c603bf5d8739b575cbfb2e9353
Ada <adrianna.kucinska@onet.eu>
2017-07-16 23:18:40 +0200

    #4 File with the first solution blocked invalid moves

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,12 +1,24 @@
-def tictactoe (string)
+def tictactoe
+
   board = ["a1","b1","c1","a2","b2","c2","a3","b3","c3"]
   used_moves = []
 
   puts "Gracz X"
   puts "Podaj miejsce ruchu, np. a1, b3:"
-  selected_move=gets.chomp
+  while
+    
+  selected_move=gets.chomp.to_s
   selected_move.downcase
-  used_moves.push(selected_move)
-  puts used_moves
   
-end 
+    if used_moves.include? selected_move
+      puts "Zajęte miejsce"
+    elsif board.include?  selected_move
+      used_moves.push(selected_move)
+    else       
+      puts "Zły ruch!"
+    end
+
+    puts used_moves
+  end
+
+end
