530f2ed33cfc4f1cb3a1da9da5f7ded0453bc2f4
Ada <adrianna.kucinska@onet.eu>
2017-09-20 15:44:01 +0200

    #9 Create display_board method

--- bin/start.rb
+++ bin/start.rb
@@ -10,12 +10,9 @@ while @players.include?@current_user
   puts check_taken_square(selected_move)
   puts save_move(selected_move)
   puts check_winner(@current_user)
+  puts check_draw(@current_user)
   change_active_user(@current_user)
-
-  puts "#{@board["a1"]} #{@board["a2"]}  #{@board["a3"]}"
-  puts "#{@board["b1"]} #{@board["b2"]}  #{@board["b3"]}"
-  puts "#{@board["c1"]} #{@board["c2"]}  #{@board["c3"]}"
-
+  puts display_board
 end
 
 puts "End of the game"
