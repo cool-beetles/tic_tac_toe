c6846066d761b09774ef679b7245e2e5bf5524bb
Ada <adrianna.kucinska@onet.eu>
2017-10-05 17:48:03 +0200

    #11 Add solution for encapsulation's task without No.1

--- bin/start.rb
+++ bin/start.rb
@@ -8,20 +8,23 @@ while game_on
   puts "Player" + game.current_user
   puts "Take square, e.g. a1, b3:"
   selected_move = gets.chomp
-  puts game.check_taken_square(selected_move)
-  game.save_move(selected_move)
 
-  winner_info = game.check_winner
-
-  if winner_info
-    game_on = false
-    puts winner_info
-  else 
-    puts game.check_draw
-    game.change_active_user
+  if !(game.save_move(selected_move))
+    puts "Ups.. Try again. Allowed moves: a1, a2, a3, b1, b2, b3, c1, c2, c3"
+    !(game.change_active_user)
   end
 
-  puts game.display_board
+  winner_info = game.display_final_status
+
+    if winner_info
+      game_on = false
+      puts winner_info
+    else 
+      game.change_active_user
+    end
+
+    puts game.display_board
+
 end
 
 puts "End of the game"
--- tictactoe.rb
+++ tictactoe.rb
@@ -16,19 +16,36 @@ class Game
   end
 
   def current_user
-    @current_user    
-  end
-
-  def check_taken_square(selected_move)    
-    "Ivalid move!" if (@board[selected_move] == "X") || (@board[selected_move] == "O")
+    @current_user
   end
 
   def save_move(selected_move)
-    if @board.include?(selected_move)
+    if check_taken_square(selected_move) == false
+      @board.include?(selected_move)
       @board[selected_move] = @current_user
-    else
-      "Allowed moves #{@board.keys}"
     end
+  end
+
+  def display_final_status
+    if check_winner
+      "The winner is: #{@current_user}"
+    elsif check_draw
+      "It's a draw"
+    end
+  end
+
+  def change_active_user
+    @current_user == @players[0] ? @current_user = @players[1] : @current_user = @players[0]
+  end
+
+  def display_board
+  "\n 1 2 3 \na#{@board["a1"]} #{@board["a2"]} #{@board["a3"]}\nb#{@board["b1"]} #{@board["b2"]} #{@board["b3"]}\nc#{@board["c1"]} #{@board["c2"]} #{@board["c3"]}"
+  end
+
+  private
+
+  def check_taken_square(selected_move)
+    (@board[selected_move] == "X") || (@board[selected_move] == "O")
   end
 
   def check_winner
@@ -41,20 +58,11 @@ class Game
     [@board["a1"], @board["b2"], @board["c3"]],
     [@board["c1"], @board["b2"], @board["a3"]]]
     
-    if win_combinations.each {|row| row.all? {|move| @board[move] == @current_user}}
-      "The winner is: #{@current_user}"
+    if win_combinations.any? {|row| row.all? {|move| @board[move] == @current_user}}
     end
   end
 
   def check_draw
-    "It's a draw" if @board.all? {|key, value| value != ""}
-  end
-
-  def change_active_user
-    @current_user == @players[0] ? @current_user = @players[1] : @current_user = @players[0]
-  end
-
-  def display_board
-  "\n 1 2 3 \na#{@board["a1"]} #{@board["a2"]} #{@board["a3"]}\nb#{@board["b1"]} #{@board["b2"]} #{@board["b3"]}\nc#{@board["c1"]} #{@board["c2"]} #{@board["c3"]}"
+    @board.all? {|key, value| value != ""}
   end
 end
